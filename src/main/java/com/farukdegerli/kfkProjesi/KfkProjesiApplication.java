4ff3cc28d3f156455e965ed82df3d5c03ccb298f
faruk degerli <farukdegerli@outlook.com.tr>
2021-11-22 01:47:34 +0300

    Rest apilerin çalışan versiyonu

---
+++ src/main/java/com/farukdegerli/kfkProjesi/KfkProjesiApplication.java
@@ -0,0 +1,19 @@
+package com.farukdegerli.kfkProjesi;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KfkProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfkProjesiApplication.class, args);
+	}
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+
+	}
+}
---
+++ src/main/java/com/farukdegerli/kfkProjesi/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.farukdegerli.kfkProjesi.service;
+
+
+import com.farukdegerli.kfkProjesi.dto.CustomerReadDto;
+import com.farukdegerli.kfkProjesi.dto.CustomerWriteDto;
+import com.farukdegerli.kfkProjesi.dto.Search;
+import com.farukdegerli.kfkProjesi.util.MyPage;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerService {
+
+    CustomerReadDto getById(Long id);
+    CustomerReadDto save(CustomerWriteDto customerWriteDto);
+    CustomerReadDto update(CustomerWriteDto customerWriteDto,Long id);
+    Boolean delete(Long id);
+    MyPage<CustomerReadDto> searchAll(Pageable pageable, Search search);
+}
